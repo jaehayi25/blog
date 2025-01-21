a53f288769a88c345db3feff12783e895cf15c77
Jaeha Yi <jaehaha1@gmail.com>
2025-01-20 23:06:10 -0700

    made edits for post2

--- src/content/blog/ames.ts
+++ src/content/blog/ames.ts
@@ -8,28 +8,27 @@ export const ames: BlogPost = {
   content: [
     {
       type: "paragraph",
-      text: `When I interned for the BMW Technology Office in Mountain View, California, in summer 2023, I would pass by an interesting complex of buildings every morning while going to work. 
-      Driving along US-101, south of the San Francisco Bay, I saw tall white and gray buildings in an organized layout, with the occasional trademark NASA logo revealing their scientific purpose.`
+      text: `When I interned for BMW's Technology Office in Mountain View, California, in summer 2023, I would pass by an interesting complex of buildings every morning while going to work. Tall white and gray buildings stood neatly by US-101, painted with the occasional trademark NASA logo... It was the Ames Research Center in the heart of Silicon Valley.`
     }, 
     {
         type: "paragraph",
-        text: `While reading <i>When the Heavens Went on Sale</i> by Ashlee Vance, it was fascinating to learn that Ames was a research center that played an crucial role in the Apollo, Mercury, and Gemini missions (back in the 1960s) and had recently transformed into one of the most innovative NASA facilities embracing youthful silicon valley ideals. The specifics of Ames's developments are in the book, but I wanted to highlight more of the <strong>rules and the organizational culture</strong> at Ames: `
+        text: `While reading <i>When the Heavens Went on Sale</i> by Ashlee Vance, it was fascinating to learn that Ames played a crucial role in the Apollo, Mercury, and Gemini missions (back in the 1960s) and had recently transformed into one of the most innovative NASA facilities embracing youthful Silicon Valley ideals. The specifics of Ames's developments are in the book, but I wanted to highlight more of the <strong>rules and the organizational culture</strong> at Ames: `
     }, 
     {
         type: "paragraph",
-        text: `- Embrace the "moral obligation" to settle the solar system (59). Worden recruited like-minded idealists with his fascinating vision of the future, and his ambition fostered a great desire for efficiency and success. One such achievement was creating a $3 million lunar lander prototype, which could have taken "billions of dollars if done to NASA's standards" (54). `
+        text: `1. Embrace the "moral obligation" to settle the solar system (59). Worden recruited like-minded idealists with his fascinating vision of the future, and his ambition fostered a great desire for efficiency and success. One such achievement was creating a $3 million lunar lander prototype, which could have taken "billions of dollars if done to NASA's standards" (54). `
     }, 
     {
         type: "paragraph",
-        text: `- "The idea of team players is... a way of saying 'Don't do anything. Be a team player.'" (53). According to Worden, being a team player meant complacency to traditional thinking and the ways government typically handled things, which were often slow, expensive, and burdened by lobbyists and bureaucratic procedures.`
+        text: `2. "The idea of team players is... a way of saying 'Don't do anything. Be a team player'" (53). According to Worden, being a team player meant complacency to traditional thinking and the ways government typically handled things, which were often slow, expensive, and burdened by lobbyists and bureaucratic procedures.`
     }, 
     {
         type: "paragraph",
-        text: `- "Never... let your enemies get you on the details." Worden's methods made a lot of enemies, and he made sure to hide weaknesses when making deals and networking with Silicon Valley peers, including other tech startups and Google.`
+        text: `3. "Never... let your enemies get you on the details" (61). Worden's methods, while innovative, made a lot of enemies, and he made sure to hide weaknesses when making deals and networking with Silicon Valley peers, including other tech startups and Google.`
     }, 
     {
         type: "paragraph",
-        text: `Ames is a good example of how organizational culture can drive innovation and how much an organization can grow and achieve. And the book does a good job of explaining this in detail... It's the people—the eccentric engineers, daring researchers, and the ambitious and commanding Ames director Pete Worden—who manifest the energy for success and bring everything together.`
+        text: `Ames is a good example of how organizational culture can drive innovation and how much an organization can grow and achieve. And the book does a good job of explaining this in detail: It's the people—the eccentric engineers, daring researchers, and the ambitious and commanding Ames director Pete Worden—who manifest the energy for success and bring everything together.`
     }
   ],
   tags: ["Technology", "Industry"]
